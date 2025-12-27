818cee3ad4b21e52c264acdb98982bf9cc0126d6
epen3420 <epen3420@gmail.com>
2025-12-27 11:29:43 +0900

    月の不正な値を検出

--- main.c
+++ main.c
@@ -9,6 +9,11 @@ int main(void){
   printf("現在の日にちを入力してください:\n");
   scanf("%d", &day);
 
+  if (month < 1 || month > 12){
+    printf("月数が不正です。もう一度やり直してください。\n");
+    return 0;
+  }
+
   printf("今日は %d月 %d日です\n", month, day);
 
   return 0;
